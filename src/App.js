629b0c55ea6d31c82dcc0c01f76aed654d34fe97
fang-zezhang <fang.zezhang@navercorp.com>
2023-06-28 16:55:53 +0800

    增加 gui 库, 用于调试界面

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import Sun from "./view/1_Sun";
 import OrbitControls from "./view/2_OrbitControls";
 import ArrayCube from "./view/3_ArrayCube";
 import PhongAndSetRenderer from "./view/4_PhongAndSetRenderer";
+import GuiLib from "./view/5_GuiLib";
 
 function App() {
-  const list = ['First3D', 'Sun', 'OrbitControls', 'ArrayCube', 'PhongAndSetRenderer'];
+  const list = ['First3D', 'Sun', 'OrbitControls', 'ArrayCube', 'PhongAndSetRenderer', 'GuiLib'];
   const map = {
     First3D,
     Sun,
     OrbitControls,
     ArrayCube,
     PhongAndSetRenderer,
+    GuiLib,
   };
 
   const [componentName, setComponentName] = useState('First3D');
--- src/view/5_GuiLib.js
+++ src/view/5_GuiLib.js
@@ -1,9 +1,23 @@
 import React, {useRef, useEffect} from 'react';
 import * as THREE from 'three';
+import { GUI } from 'dat.gui';
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
-export default function _PhongAndSetRnderer() {
+export default function _GuiLib() {
   const div = useRef(null);
+
+  let res;
+  let gui;
+  const initGUI = () => {
+    gui = new GUI();
+    gui.domElement.style.right = '0px';
+    gui.domElement.style.width = '300px';
+
+    const exampleObj = {
+      example: 30,
+    };
+    gui.add(exampleObj, 'example', 0, 100);
+  };
 
   const initRenderer = () => {
     const scene = new THREE.Scene();
@@ -18,12 +32,31 @@ export default function _PhongAndSetRnderer() {
     mesh.position.set(0, 0, 0);
     scene.add(mesh);
 
+    const obj = {
+      color: 0xffffff,
+    };
+    const materialFolder = gui.addFolder('材质'); // gui 增加分组
+    materialFolder.close(); // 关闭菜单
+    materialFolder.add(obj, 'color', 0x000000, 0xfffffff).name('蒙层颜色').onChange((value) => {
+      mesh.material.color.set(value);
+    });
+
+    // 交互界面是拖动条
+    gui.add(mesh.position, 'x', 0, 180).onChange((value) => {
+      mesh.position.x = value * 0.1;
+    });
+    gui.add(mesh.position, 'y', [0, 50, 100, 180]);  // 交互界面是下拉菜单
+    gui.add(mesh.position, 'z', 0, 180);
+
     const camera = new THREE.PerspectiveCamera(30, 800 / 500, 1, 2000);
     camera.position.set(200, 150, 200);
     camera.lookAt(mesh.position);
 
     const pointLight = new THREE.PointLight(0xffffff, 1.0);
     const pointLightHelper = new THREE.PointLightHelper(pointLight);
+
+    materialFolder.add(pointLight, 'intensity', 0, 2.0).name('光照强度(流明)');
+
     pointLight.position.set(200, 100, 100);
     scene.add(pointLight);
     scene.add(pointLightHelper);
@@ -44,14 +77,22 @@ export default function _PhongAndSetRnderer() {
     function animation() {
       mesh.rotateY(0.01);
       renderer.render(scene, camera);
-      requestAnimationFrame(animation);
+      res = requestAnimationFrame(animation);
     }
-    animation();
+    res = animation();
+    gui.add({bool: true}, 'bool').name('mesh 是否旋转').onChange((value) => {
+      if (value) {
+        res = requestAnimationFrame(animation);
+      } else {
+        cancelAnimationFrame(res);
+      }
+    });
 
     return renderer;
   };
 
   useEffect(() => {
+    initGUI();
     const node = div.current;
     const renderer = initRenderer();
 
@@ -59,6 +100,8 @@ export default function _PhongAndSetRnderer() {
 
     return () => {
       node.removeChild(renderer.domElement);
+      gui.destroy();
+      cancelAnimationFrame(res);
     }
   }, []);
 
