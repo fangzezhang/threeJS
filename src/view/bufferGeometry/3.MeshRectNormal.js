c6d69bb79546b963f1e7ef4b6ca22e8992328510
fang-zezhang <fang.zezhang@navercorp.com>
2023-06-30 17:35:21 +0800

    增加法线: 改变材质后, light 下显示 geometry

--- src/view/bufferGeometry/3.MeshRectNormal.js
+++ src/view/bufferGeometry/3.MeshRectNormal.js
@@ -2,40 +2,47 @@ import React, {useEffect, useRef} from 'react';
 import * as THREE from 'three';
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
-export default function MeshRect() {
+export default function MeshRectNormal() {
   const div = useRef(null);
 
+  /*
+  * 不设置法线, 改变材质, geometry 无法显示
+  * */
   const initRenderer = () => {
     const scene = new THREE.Scene();
 
-    /*const vertices = new Float32Array([
-      0, 0, 0, //顶点1坐标
-      80, 0, 0, //顶点2坐标
-      80, 80, 0, //顶点3坐标
-      0, 0, 0, //顶点4坐标   和顶点1位置相同
-      80, 80, 0, //顶点5坐标  和顶点3位置相同
-      0, 80, 0, //顶点6坐标
-    ]);
-    */
-    // 通过 index 去除重复坐标
     const vertices = new Float32Array([
-      0, 0, 0, //顶点1坐标
-      80, 0, 0, //顶点2坐标
-      80, 80, 0, //顶点3坐标
-      0, 80, 0, //顶点6坐标
+      0, 0, 0,
+      80, 0, 0,
+      80, 80, 0,
+      0, 80, 0,
     ]);
-    const geometry = new THREE.BufferGeometry();
     const attribute = new THREE.BufferAttribute(vertices, 3);
     const index = new Uint16Array([
-      0, 1, 2, 0, 2, 3
+      0, 1, 2, 0, 2, 3,
     ]);
-    geometry.index = new THREE.BufferAttribute(index, 1);  // 配置顶点索引, 减少重复坐标
-    geometry.setAttribute('position', attribute);
-    const material = new THREE.MeshBasicMaterial({
-      color: 0x00ffff,
+    const normals = new Float32Array([
+      0, 0, 1,
+      0, 0, 1,
+      0, 0, 1,
+      0, 0, 1,
+    ]);
+    const geometry = new THREE.BufferGeometry();
+    geometry.attributes.position = attribute;
+    // 使用了顶点索引, 每个法线数据和顶点位置数据一一对应:
+    geometry.attributes.normal = new THREE.BufferAttribute(normals, 3);
+    geometry.index = new THREE.BufferAttribute(index, 1);
+    const material = new THREE.MeshLambertMaterial({
+      color: 0x00ff00
     });
     const mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
+
+    const pointLight = new THREE.PointLight('white', 2.0);
+    const pointLightHelper = new THREE.PointLightHelper(pointLight);
+    pointLight.position.set(0, 100, 100);
+    scene.add(pointLight);
+    scene.add(pointLightHelper);
 
     const camera = new THREE.PerspectiveCamera(30, 800 / 500, 1, 2000);
     camera.position.set(200, 200, 300);
